4b5ee864461f9f4d73565915e263bd9944fa95e8
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-10-28 12:22:03 +0100

    Fix : collection name error

--- src/main/java/com/greentechinnovators/entity/Data.java
+++ src/main/java/com/greentechinnovators/entity/Data.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
-@Document(collation = "data")
+@Document(collection = "data")
 public class Data {
     @Id
     private String id;
