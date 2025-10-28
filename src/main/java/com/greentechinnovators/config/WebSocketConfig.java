51d8d50e4cb86130878cf0aa0dd97bfeeccd1be8
youssefElhoubi <elhoubiyoussef@gmail.com>
2025-10-28 11:25:52 +0100

    Add WebSocket support for data management and enhance data controller

---
+++ src/main/java/com/greentechinnovators/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.greentechinnovators.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Client connects to this endpoint
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // Messages prefixed with /app go to controller
+        registry.setApplicationDestinationPrefixes("/app");
+        // Messages prefixed with /topic go to subscribers
+        registry.enableSimpleBroker("/topic");
+    }
+}
--- src/main/java/com/greentechinnovators/controllers/dataController.java
+++ src/main/java/com/greentechinnovators/controllers/dataController.java
@@ -3,6 +3,7 @@ package com.greentechinnovators.controllers;
 import com.greentechinnovators.entity.Data;
 import com.greentechinnovators.service.DataService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,12 +16,14 @@ public class dataController {
     public dataController(DataService dataService) {
         this.dataService = dataService;
     }
+
     @GetMapping
-    public List<Data> all(){
+    public List<Data> all() {
         return dataService.all();
     }
+
     @PostMapping
-    public ResponseEntity<?> add(@RequestBody Data data){
+    public ResponseEntity<?> add(@RequestBody Data data) {
         dataService.add(data);
         return ResponseEntity.ok("new data was added");
     }
