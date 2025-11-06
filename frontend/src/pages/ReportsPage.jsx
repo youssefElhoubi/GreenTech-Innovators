0be65292ec4e291803b90a22473c24fff07c73f3
MohamedMoustir <itsmoustir@gmail.com>
2025-11-06 14:58:49 +0100

    SDGOTL-77: Ajouter tests et vérifications endpoints Delivery avec pagination

--- frontend/src/pages/ReportsPage.jsx
+++ frontend/src/pages/ReportsPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-
+import Swal from 'sweetalert2';
 function ReportsPage() {
   const [reports, setReports] = useState([])
   const [loading, setLoading] = useState(true)
